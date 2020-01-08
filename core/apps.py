16bb56a60ba5cca6920ab3025fa9ffa6ba66dd7c
노종혁 <sofaissofa@icloud.com>
2020-01-08 12:26:13 +0900

    settings 변경 및 base 수정

--- core/apps.py
+++ core/apps.py
@@ -3,3 +3,34 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = 'core'
+    verbose_name = '핵심 기능'
+
+from suit.apps import DjangoSuitConfig
+from suit.menu import ParentItem, ChildItem
+
+
+class SuitConfig(DjangoSuitConfig):
+    layout = 'horizontal'
+
+    menu = (
+        ParentItem('조직 관리', children=[
+            ChildItem(model='auth.user'),
+            ChildItem(model='auth.group'),
+        ], icon='fa fa-users'),
+        ParentItem('종목 관리', children=[
+            ChildItem(model='core.sport'),
+        ], icon='fa fa-users'),
+        ParentItem('업체 관리', children=[
+            ChildItem(model='core.academy'),
+        ], icon='fa fa-users'),
+        ParentItem('코치 관리', children=[
+            ChildItem(model='core.coach'),
+        ], icon='fa fa-users'),
+        ParentItem('레슨 관리', children=[
+            ChildItem(model='core.lesson'),
+        ], icon='fa fa-users'),
+        ParentItem('위치 관리', children=[
+            ChildItem(model='core.district'),
+            ChildItem(model='core.location'),
+        ], icon='fa fa-leaf'),
+    )
