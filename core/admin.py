fc5644f9797a6503a755da325e9f81931d0b018b
JinHyeok Park <jhp@example.com>
2020-01-08 16:58:03 +0900

    added Models to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Academy)
+admin.site.register(Lesson)
+admin.site.register(Coach)
+admin.site.register(Sport)
+admin.site.register(District)
+admin.site.register(Location)
