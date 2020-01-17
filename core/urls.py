9b1dccf7ac90ce6ad1b83353bf6a889df21974a6
노종혁 <sofaissofa@icloud.com>
2020-01-17 15:31:59 +0900

    sports_list html 생성

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib.auth.views import LoginView
 
 urlpatterns = [
+
     path('login/', LoginView.as_view(), name='login'),
     path('', views.index, name='index'),
 ]
