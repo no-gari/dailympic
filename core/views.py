e5c082122a17ffbc79be15c10062e340d6ba5965
jhp2060 <jhp2060@gmail.com>
2020-02-09 14:08:06 +0900

    media upload to && fix trivials of forms

--- core/views.py
+++ core/views.py
@@ -131,9 +131,9 @@ class LikesTemplateView(TemplateView):
 
 
 def user_create(request):
+    user_form = UserForm(request.POST or None)
+    profile_form = ProfileForm(request.POST or None)
     if request.method == 'POST':
-        user_form = UserForm(request.POST)
-        profile_form = ProfileForm(request.POST)
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save()
             profile = profile_form.save(commit=False)
@@ -141,8 +141,8 @@ def user_create(request):
             profile.save()
             return redirect('login')
     ctx = {
-        'user_form': UserForm(),
-        'profile_form': ProfileForm(),
+        'user_form': user_form,
+        'profile_form': profile_form,
     }
     return render(request, 'user/test_signup.html', ctx)
 
